453c82f64e1696e9b317c9f5b611ba2165a09594
Izzat Rizal <86939634+ejjat0909@users.noreply.github.com>
2023-05-03 20:18:02 +0800

    searchpage, time lapse

--- lib/screens/home_page/home_page_screen.dart
+++ lib/screens/home_page/home_page_screen.dart
@@ -1,13 +1,22 @@
 import 'package:attendance_system/bloc/attendance_bloc.dart';
 import 'package:attendance_system/bloc/user_bloc.dart';
 import 'package:attendance_system/constant.dart';
+import 'package:attendance_system/helpers/general_method.dart';
 import 'package:attendance_system/model/attendance/attendance_list_response_model.dart';
 import 'package:attendance_system/model/attendance/attendance_model.dart';
 import 'package:attendance_system/model/default_response_model.dart';
 import 'package:attendance_system/model/user/user_model.dart';
+import 'package:attendance_system/public_components/space.dart';
+import 'package:attendance_system/public_components/theme_snack_bar%20copy.dart';
+import 'package:attendance_system/screens/add_attendance/add_attendance_screen.dart';
+import 'package:attendance_system/screens/search_page/search_page_screen.dart';
 import 'package:attendance_system/screens/sign_in/sign_in_screen.dart';
+import 'package:attendance_system/screens/view_attendance/view_attendance_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:attendance_system/public_components/space.dart';
+import 'package:flutter_scale_tap/flutter_scale_tap.dart';
+import 'package:intl/intl.dart';
 
 class HomePageScreen extends StatefulWidget {
   final UserModel userModel;
@@ -19,6 +28,9 @@ class HomePageScreen extends StatefulWidget {
 
 class _HomePageScreenState extends State<HomePageScreen> {
   List<AttendanceModel> listAttendance = [];
+  String formattedTime = "1 second ago";
+  DateTime dateTime = DateTime.now();
+  bool isSwitchedOn = true;
 
   void updateAttendanceList(AttendanceModel attendanceModel) {
     setState(() {
@@ -26,8 +38,18 @@ class _HomePageScreenState extends State<HomePageScreen> {
     });
   }
 
+  void loadData() async {
+    AttendanceListResponseModel? response = await getAttendance();
+    if (response != null && response.isSuccess) {
+      setState(() {
+        listAttendance = response.data!;
+      });
+    }
+  }
+
   Future<AttendanceListResponseModel>? getAttendance() async {
     AttendanceBloc attendanceBloc = AttendanceBloc();
+
     // for auto update
     setState(() {});
     try {
@@ -38,11 +60,32 @@ class _HomePageScreenState extends State<HomePageScreen> {
     return AttendanceListResponseModel({'': ''});
   }
 
+  ScrollController _scrollController = ScrollController();
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     getAttendance();
+    loadData();
+    _scrollController.addListener(_onScroll);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _scrollController.dispose();
+  }
+
+  void _onScroll() {
+    //use minScrollExtent because reverse:true in listview
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      ThemeSnackBar.showSnackBar(
+          context, "You have reached the end of the list");
+      print("end scroll");
+    }
   }
 
   @override
@@ -56,38 +99,53 @@ class _HomePageScreenState extends State<HomePageScreen> {
           leading: BackButton(color: Colors.transparent),
           actions: [
             IconButton(
-                onPressed: () async {
-                  UserBloc userBloc = UserBloc();
-                  DefaultResponseModel responseModel = await userBloc.logout();
+              onPressed: () {
+                navigateTo(
+                  context,
+                  SearchPageScreen(
+                    attendanceModel: listAttendance,
+                  ),
+                );
+              },
+              icon: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+            ),
+            IconButton(
+              onPressed: () async {
+                UserBloc userBloc = UserBloc();
+                DefaultResponseModel responseModel = await userBloc.logout();
 
-                  if (responseModel.isSuccess) {
-                    if (mounted) {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(
-                          content: Text("Logged out"),
-                        ),
-                      );
-                      print("Logged out");
-                      Navigator.pushAndRemoveUntil(
-                          context,
-                          MaterialPageRoute(
-                            builder: ((context) => SignInScreen()),
-                          ),
-                          (Route<dynamic> route) => false);
-                    }
-                  } else {
+                if (responseModel.isSuccess) {
+                  if (mounted) {
                     ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(
-                        content: Text(responseModel.message),
+                      const SnackBar(
+                        content: Text("Logged out"),
                       ),
                     );
-                    print("cannot Log out");
+                    print("Logged out");
+                    Navigator.pushAndRemoveUntil(
+                        context,
+                        MaterialPageRoute(
+                          builder: ((context) => SignInScreen()),
+                        ),
+                        (Route<dynamic> route) => false);
                   }
-                },
-                icon: Icon(
-                  Icons.logout,
-                  color: Colors.black,
-                ))
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text(responseModel.message),
+                    ),
+                  );
+                  print("cannot Log out");
+                }
+              },
+              icon: Icon(
+                Icons.logout,
+                color: Colors.red,
+              ),
+            ),
           ],
           title: Text(
             "Hi ${widget.userModel.name}",
@@ -97,80 +155,140 @@ class _HomePageScreenState extends State<HomePageScreen> {
           ),
           centerTitle: true,
         ),
-        body: FutureBuilder<AttendanceListResponseModel>(
-          future: getAttendance(),
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              if (snapshot.data!.isSuccess) {
-                List<AttendanceModel> blogs = snapshot.data!.data!;
-                return ListView.builder(
-                    reverse: true,
-                    physics: BouncingScrollPhysics(),
-                    itemCount: blogs.length,
-                    itemBuilder: (context, index) {
-                      AttendanceModel blogModel = blogs[index];
-                      return Container(
-                        padding: EdgeInsets.all(10),
-                        margin: EdgeInsets.all(10),
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(5),
-                          border: Border.all(color: kPrimaryColor, width: 2),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              children: [
-                                Text(
-                                  blogModel.user!.name!,
-                                  style: TextStyle(
-                                    color: kPrimaryColor,
-                                    fontWeight: FontWeight.w500,
-                                  ),
-                                )
-                              ],
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              blogModel.title!,
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.start,
-                            ),
-                            SizedBox(height: 10),
-                            Text(
-                              blogModel.body!,
-                              textAlign: TextAlign.justify,
-                            )
-                          ],
-                        ),
-                      );
+        body: Column(
+          children: [
+            Row(
+              children: [
+                Switch(
+                  // This bool value toggles the switch.
+                  value: isSwitchedOn,
+                  activeColor: kPrimaryColor,
+                  onChanged: (bool value) {
+                    // This is called when the user toggles the switch.
+                    setState(() {
+                      isSwitchedOn = value;
                     });
-              } else {
-                return const Center(
-                  child: Text("Something went wrong."),
-                );
-              }
-            } else {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-          },
+                  },
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  "Switch on/off the toggle to change the time format",
+                  style: TextStyle(
+                    color: isSwitchedOn ? kPrimaryColor : kGrey,
+                    fontSize: 12,
+                  ),
+                )
+              ],
+            ),
+            Expanded(
+              child: FutureBuilder<AttendanceListResponseModel>(
+                future: getAttendance(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    if (snapshot.data!.isSuccess) {
+                      List<AttendanceModel> attendance = snapshot.data!.data!;
+                      return ListView.builder(
+                          controller: _scrollController,
+                          reverse: false,
+                          physics: BouncingScrollPhysics(),
+                          itemCount: attendance.length,
+                          itemBuilder: (context, index) {
+                            AttendanceModel attendanceModel = attendance[index];
+                            return ScaleTap(
+                              onPressed: () async {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          ViewAttendanceScreen(
+                                            attendanceModel: attendanceModel,
+                                          )),
+                                ).then((value) {
+                                  getAttendance(); // Reload the data after adding a new item
+                                });
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(10),
+                                margin: EdgeInsets.all(10),
+                                width: double.infinity,
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  borderRadius: BorderRadius.circular(15),
+                                  border: Border.all(
+                                      color: kPrimaryColor, width: 1),
+                                ),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.start,
+                                      children: [
+                                        Text(
+                                          attendanceModel.user!.name!,
+                                          style: TextStyle(
+                                            color: kPrimaryColor,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        )
+                                      ],
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      attendanceModel.title!,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                      textAlign: TextAlign.start,
+                                    ),
+                                    SizedBox(height: 10),
+                                    Text(
+                                      attendanceModel.body!,
+                                      textAlign: TextAlign.justify,
+                                    ),
+                                    Space(10),
+                                    Text(
+                                      isSwitchedOn
+                                          ? "1 second ago"
+                                          : DateFormat('dd MMM yyyy, h:mm a')
+                                              .format(dateTime),
+                                      style: const TextStyle(
+                                        color: kGrey,
+                                        fontSize: 10,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          });
+                    } else {
+                      return const Center(
+                        child: Text("Something went wrong."),
+                      );
+                    }
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              ),
+            ),
+          ],
         ),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.black,
           child: Icon(Icons.add, color: Colors.white),
           onPressed: () async {
             // Perform action when button is pressed
-            // Navigator.push(
-            //   context,
-            //   MaterialPageRoute(builder: (context) => AddNewBlogPage()),
-            // ).then((value) {
-            //   getBlogs(); // Reload the data after adding a new item
-            // });
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => AddAttendanceScreen()),
+            ).then((value) {
+              getAttendance(); // Reload the data after adding a new item
+            });
           },
         ),
       ),
